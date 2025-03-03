382425dd6139f59687deed57259555d2ae6ae429
manish <krmanish2512@gmail.com>
2025-03-03 13:28:04 +0530

    Added Terraform config for S3 bucket

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "deetya-vyom-03032025"
+  acl    = "private"
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
