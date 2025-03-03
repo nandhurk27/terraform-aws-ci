6c6484616389dc70a05118b1dd64f5cd7fe67f5d
nandhurk27 <nandhurk27@gmail.com>
2025-03-03 13:42:04 +0530

    Updated S3 bucket config

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "deetyavyom-03032025"
+  bucket = "deetya-vyom-03032025"
   acl    = "private"
   tags = {
     Name        = "MyBucket"
